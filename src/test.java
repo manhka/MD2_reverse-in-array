34139f3531c5f904bb9dccdb3c2af6761a6eb469
unknown <manh87835@gmail.com>
2022-03-23 13:52:21 +0700

    finish

--- src/test.java
+++ src/test.java
@@ -1,25 +1,35 @@
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class test {
 
     public static void main(String[] args) {
+//
         System.out.println("enter a arr");
-        Integer arr1[]= {1,2,3,6,99,33};
-//        int arr2[]=new int[arr1.length];
-//        for (int i = 0; i < arr1.length-1; i++) {
-//            int k=i+1;
-//            arr2[i]=arr1[arr1.length-k];
-//        }
-//        for (int number:arr2
-//             ) {
-//            System.out.println("arr2=="+number);
-//        }
-        reverse(arr1);
+//        Integer arr1[]= {1,2,3,6,99,33};
+//        reverse(arr1);
+        int arr1[]= new int[5];
+        int i=0;
+        int arr2[]=new int[arr1.length];
+        Scanner sc= new Scanner(System.in);
+        while (i<arr2.length){
+            arr1[i]= sc.nextInt();
+            i++;
+        }
+        for (int x= 0; x< arr1.length; x++) {
+            int k=x+1;
+            arr2[x]=arr1[arr1.length-k];
+        }
+        for (int number:arr2
+             ) {
+            System.out.println("arr2=="+number);
+        }
+
     }
-    static void reverse(Integer arr[]){
-        Collections.reverse(Arrays.asList(arr));
-        System.out.println(Arrays.asList(arr));
-    }
+//    static void reverse(Integer arr[]){
+//        Collections.reverse(Arrays.asList(arr));
+//        System.out.println(Arrays.asList(arr));
+//    }
     }
 
